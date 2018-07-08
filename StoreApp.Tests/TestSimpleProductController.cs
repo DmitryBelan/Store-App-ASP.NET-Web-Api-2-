c17ec622930180645bd046ce25f4957aab764f1e
Dmitriy Belan <kigojov@gmail.com>
2018-07-08 11:53:39 +0300

    Как-то в полночь, в час угрюмый, утомившись от раздумий, Задремал я над страницей кода одного

--- StoreApp.Tests/TestSimpleProductController.cs
+++ StoreApp.Tests/TestSimpleProductController.cs
@@ -30,38 +30,7 @@ namespace StoreApp.Tests
             Assert.AreEqual(testProducts.Count, result.Count);
         }
 
-        //[TestMethod]
-        //public void GetProduct_ShouldReturnCorrectProduct()
-        //{
-        //    var testProducts = GetTestProducts();
-        //    var controller = new SimpleProductController(testProducts);
-
-        //    var result = controller.GetProduct(4) as OkNegotiatedContentResult<Product>;
-        //    Assert.IsNotNull(result);
-        //    Assert.AreEqual(testProducts[3].Name, result.Content.Name);
-        //}
-
-        //[TestMethod]
-        //public async Task GetProductAsync_ShouldReturnCorrectProduct()
-        //{
-        //    var testProducts = GetTestProducts();
-        //    var controller = new SimpleProductController(testProducts);
-
-        //    var result = await controller.GetProductAsync(4) as OkNegotiatedContentResult<Product>;
-        //    Assert.IsNotNull(result);
-        //    Assert.AreEqual(testProducts[3].Name, result.Content.Name);
-        //}
-
-        //[TestMethod]
-        //public void GetProduct_ShouldNotFindProduct()
-        //{
-        //    var controller = new SimpleProductController(GetTestProducts());
-
-        //    var result = controller.GetProduct(999);
-        //    Assert.IsInstanceOfType(result, typeof(NotFoundResult));
-        //}
-
-        private List<Product> GetTestProducts()
+       private List<Product> GetTestProducts()
         {
             var testProducts = new List<Product>();
             testProducts.Add(new Product { Id = 1, Name = "Demo1", Price = 1 });
